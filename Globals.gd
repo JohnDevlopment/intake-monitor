14797e1555aef1f32a24a6b1d7878087064180b2
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-06-30 09:35:56 -0500

    Add plus icon texture

--- Globals.gd
+++ Globals.gd
@@ -4,6 +4,7 @@ signal request_save()
 
 const DELETE_BUTTON_TEXTURE := preload('res://assets/textures/delete_icon.png')
 const EDIT_BUTTON_TEXTURE := preload('res://assets/textures/pencil.png')
+const ADD_BUTTON_TEXTURE := preload('res://assets/textures/add_icon.png')
 
 func request_save() -> void:
 	emit_signal('request_save')
