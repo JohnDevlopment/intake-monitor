aae2a51eef1f3c5cd0b37d9c4fb84e0caa684056
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-07-01 10:47:42 -0500

    Implement editing intake monitor entries

--- scenes/IntakeMonitor.gd
+++ scenes/IntakeMonitor.gd
@@ -2,7 +2,7 @@ extends MarginContainer
 
 signal closing()
 
-const DELETE_BUTTON_TEXTURE := preload('res://assets/textures/delete_icon.png')
+enum ButtonID {EDIT, DELETE}
 
 export var intake_name := ''
 export var desired_max := 1000
@@ -11,6 +11,7 @@ export(String, 'g', 'mg') var unit := 'mg'
 onready var entries : Tree = get_node('%Entries')
 onready var sum_label = $AspectRatioContainer/VSplitContainer/VBoxContainer/SumLabel
 onready var delay: Timer = $Delay
+onready var edit_item: LineEdit = $'%EditItem'
 
 var items := {}
 var sum := 0
@@ -25,6 +26,8 @@ func _ready() -> void:
 	
 	entries.set_column_title(0, "Food/Drink")
 	entries.set_column_title(1, "Amount")
+	
+	edit_item.set_as_toplevel(true)
 	
 	# Name of the intake being monitored
 	if not intake_name.empty():
@@ -78,6 +81,7 @@ func serialize() -> Dictionary:
 	return data
 
 func _on_entry_added(_name: String, amount: String, update: bool = true) -> void:
+	# Create the tree item and insert the column texts
 	var item := entries.create_item(entries)
 	items[item.get_instance_id()] = {
 		name = _name,
@@ -85,24 +89,67 @@ func _on_entry_added(_name: String, amount: String, update: bool = true) -> void
 	}
 	item.set_text(0, _name)
 	item.set_text(1, "%s %s" % [amount, unit])
-	item.add_button(1, DELETE_BUTTON_TEXTURE)
+	item.add_button(0, Globals.EDIT_BUTTON_TEXTURE, ButtonID.EDIT)
+	item.add_button(1, Globals.EDIT_BUTTON_TEXTURE, ButtonID.EDIT)
+	item.add_button(1, Globals.DELETE_BUTTON_TEXTURE, ButtonID.DELETE)
+	
+	# Recalculate sum
 	if update:
 		_should_recalculate = true
 	_update_amount()
+	
 	Globals.request_save()
 
-func _on_Entries_button_pressed(item: TreeItem, column: int, _id: int) -> void:
+func _on_Entries_button_pressed(item: TreeItem, column: int, id: int) -> void:
 	if not delay.is_stopped(): return
-	var item_id := item.get_instance_id()
-	if column == 1:
+	
+	if id == ButtonID.DELETE:
+		# Delete entry
+		var item_id := item.get_instance_id()
 		items.erase(item_id)
 		item.free()
 		call_deferred('_update_amount')
 		delay.start()
 		Globals.request_save()
 		_should_recalculate = true
+	else:
+		# Edit entry
+		var item_rect := entries.get_item_area_rect(item, column)
+		item_rect.position += entries.rect_global_position + Vector2(8, 0)
+		edit_item.rect_position = item_rect.position
+		edit_item.rect_size = item_rect.size
+		
+		set_meta('edited_item', item)
+		set_meta('edited_column', column)
+		
+		$ExcScreen.show()
+		
+		# Initialize the text in the line edit and then display it
+		edit_item.text = item.get_text(column)
+		edit_item.show_modal()
+		edit_item.grab_focus()
+		
+		edit_item.connect('hide', self, '_on_cancelled_editing_treeitem',
+						[], CONNECT_ONESHOT)
+
+func _on_cancelled_editing_treeitem() -> void:
+	$ExcScreen.hide()
 
 func _on_ConfirmationDialog_confirmed() -> void:
 	get_parent().remove_child(self)
 	emit_signal('closing')
 	queue_free()
+
+func _on_edited_tree_item(new_text: String) -> void:
+	# Hide the lineedit and the color rect
+	var le : LineEdit = $'%EditItem'
+	le.release_focus()
+	le.hide()
+	le.text = ''
+	$ExcScreen.hide()
+	
+	# Modify the item's text
+	var item : TreeItem = get_meta('edited_item')
+	item.set_text(get_meta('edited_column'), new_text)
+	
+	Globals.request_save()
