54ccde29c3a46ddf3796fce18e419376770a00cb
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-07-05 06:44:48 -0500

    Error dialog function in Globals singleton

--- Globals.gd
+++ Globals.gd
@@ -8,3 +8,14 @@ const ADD_BUTTON_TEXTURE := preload('res://assets/textures/add_icon.png')
 
 func request_save() -> void:
 	emit_signal('request_save')
+
+func show_error(parent: Node, message: String) -> void:
+	assert(is_instance_valid(parent) and parent.is_inside_tree(), "invalid parent")
+	var dlg := AcceptDialog.new()
+	dlg.dialog_text = message
+	dlg.connect('hide', self, '_on_dialog_hide', [dlg])
+	parent.add_child(dlg)
+	dlg.popup_centered()
+
+func _on_dialog_hide(dlg: Node) -> void:
+	dlg.queue_free()
