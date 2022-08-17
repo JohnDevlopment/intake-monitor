c7ce45ca15612c5ea0f7ed5bdd37c377afa0dc55
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-08-16 21:14:33 -0500

    Update ErrorLabel.tscn

--- scenes/ErrorLabel.gd
+++ scenes/ErrorLabel.gd
@@ -3,9 +3,9 @@ extends Label
 func _ready() -> void:
 	text = ''
 
-func set_error(e: String) -> void:
+func set_error(e: String, time: float = -1) -> void:
 	text = e
-	$Timer.start()
+	$Timer.start(time)
 
 func _on_Timer_timeout() -> void:
 	text = ''
