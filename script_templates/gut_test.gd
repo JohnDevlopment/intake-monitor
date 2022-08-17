317394d5ca001fb09c7d452cd23a619f897769af
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-08-16 23:32:23 -0500

    Add GUT plugin
    * Plugin for unit testing

---
+++ script_templates/gut_test.gd
@@ -0,0 +1,34 @@
+extends 'res://addons/gut/test.gd'
+
+class TestInnerClass:
+	extends 'res://addons/gut/test.gd'
+	
+	func after_all() -> void:
+		pass
+
+	func after_each() -> void:
+		pass
+
+	func before_all() -> void:
+		pass
+
+	func before_each() -> void:
+		pass
+
+	func test_METHODNAME():
+		pass
+
+func after_all() -> void:
+	pass
+
+func after_each() -> void:
+	pass
+
+func before_all() -> void:
+	pass
+
+func before_each() -> void:
+	pass
+
+func test_METHODNAME():
+	pass
