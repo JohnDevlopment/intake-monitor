15097edd3b59babf3cfed3bbbd57e96eab8e4665
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-08-18 08:22:44 -0500

    Refactor tree item edit control
    * Connect signals in code
    * Code optimizations and comments

--- scenes/EditTreeItem.gd
+++ scenes/EditTreeItem.gd
@@ -10,6 +10,8 @@ var _validate : FuncRef
 func _ready() -> void:
 	set_as_toplevel(true)
 	hide()
+	connect('text_entered', self, '_on_edited_treeitem')
+	connect('hide', self, '_cancel')
 
 func activate(tree: Tree, item: TreeItem, column: int):
 	var item_rect := tree.get_item_area_rect(item, column)
@@ -24,16 +26,26 @@ func activate(tree: Tree, item: TreeItem, column: int):
 	show_modal()
 	grab_focus()
 
+func deactivate() -> void:
+	release_focus()
+	hide()
+
 func set_validate_callback(node: Node, function: String) -> void:
 	_validate = funcref(node, function)
 	assert(_validate.is_valid())
 
+func _cancel() -> void:
+	deactivate()
+	emit_signal('cancelled_edit')
+
+# Signals
+
+# Connected at _ready to "text_entered" signal
 func _on_edited_treeitem(new_text: String) -> void:
 	assert(edited_column >= 0)
 	assert(_edited_item)
 	
-	release_focus()
-	hide()
+	deactivate()
 	
 	if is_instance_valid(_validate) and _validate.is_valid():
 		if not _validate.call_func(new_text):
@@ -46,6 +58,3 @@ func _on_edited_treeitem(new_text: String) -> void:
 	edited_column = -1
 	
 	emit_signal('edited_tree_item', new_text)
-
-func _on_EditTreeItem_hide() -> void:
-	emit_signal('cancelled_edit')
