e3a1258f830d45fc97b95370e433226d19e786cb
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2022-08-19 19:46:25 -0500

    Add git-changelog.sh

---
+++ git-changelog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "# Changelog"
+echo
+
+declare -r USER=JohnDevlopment
+declare -r PROJECT=factory-level
+
+git log ${1:?provide revision range} \
+    --pretty=format:"* [view commit](https://github.com/$USER/$PROJECT/commit/%H)&bull; %s " \
+    --reverse
